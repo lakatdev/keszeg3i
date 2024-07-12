188b69e9fc1cec7a18224cb02bec996175d1eb6d
fodor.robert <fodor.robert@szikramozgalom.hu>
2024-07-12 14:23:08 +0200

    whatevs

--- src/line.cpp
+++ src/line.cpp
@@ -51,9 +51,11 @@ string Line::parseString(int after)
     return result;
 }
 
-void Line::replaceAll(std::string& str, const std::string& from, const std::string& to) {
+void Line::replaceAll(std::string& str, const std::string& from, const std::string& to)
+{
     size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+    while((start_pos = str.find(from, start_pos)) != std::string::npos)
+    {
         str.replace(start_pos, from.length(), to);
         start_pos += to.length();
     }
