bf3be6469861900e129dd0b9db43acf4d26db304
fodor.robert <fodor.robert@szikramozgalom.hu>
2024-07-17 16:14:22 +0200

    save implemented

--- include/instructions/save.hpp
+++ include/instructions/save.hpp
@@ -7,6 +7,9 @@ namespace Instructions
 {
     class Save : public Instruction
     {
+        private:
+        void saveStringToFile(const string& str, const string& filename);
+        void saveArrayToFile(const string& arr, const string& filename);
         public:
         Save(ControlFlow& controlFlow, Memory& memory);
         void execute(Line line);
--- src/instructions/save.cpp
+++ src/instructions/save.cpp
@@ -2,6 +2,8 @@
 #include <keszeg3i.hpp>
 #include <memory.hpp>
 #include <controlflow.hpp>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -11,8 +13,56 @@ Instructions::Save::Save(ControlFlow& controlFlow, Memory& memory): Instruction(
     keyPositions = {0};
 }
 
+void Instructions::Save::saveStringToFile(const string& str, const string& filename)
+{
+    std::ofstream file(filename, ios::binary);
+    if (file.is_open())
+    {
+        file.write(str.data(), str.size());
+        file.close();
+    }
+    else
+    {
+        Keszeg3i::error("Could not open file");
+    }
+}
+
+void Instructions::Save::saveArrayToFile(const string& arr, const string& filename)
+{
+    ofstream file(filename, ios::binary);
+    if (file.is_open())
+    {
+        for (int i = 0; i < memory.getArraySize(arr); i++)
+        {
+            int element = memory.getArrayElement(arr, i);
+            file.write(reinterpret_cast<const char*>(&element), sizeof(element));
+        }
+        file.close();
+    } 
+    else
+    {
+        Keszeg3i::error("Could not open file");
+    }
+}
+
 void Instructions::Save::execute(Line line)
 {
     vector<string> args = line.getTokens();
-    // TODO: Implement
+    if (args.size() != 3)
+    {
+        Keszeg3i::error("Usage: save X Y");
+    }
+
+    if (memory.isString(args[1]))
+    {
+        saveStringToFile(memory.getString(args[1]), line.parseString(2));
+    }
+    else if (memory.isArray(args[1]))
+    {
+        saveArrayToFile(args[1], line.parseString(2));
+    }
+    else
+    {
+        Keszeg3i::error("Variable is not a string or an array");
+    }
 }
